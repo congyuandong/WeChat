788d0ed0037f58ac65b627ee0deb3501c9c3946e
congyuandong <congyuandong@gmail.com>
2014-10-31 01:07:15 +0800

    remove space

--- express/views.py
+++ express/views.py
@@ -20,28 +20,28 @@ def index(request):
 
 @csrf_exempt
 def token(request):
-    if request.method=='GET':
-        response=HttpResponse(checkSignature(request))
-        return response
-    else:
-        return HttpResponse('Hello World')
+	if request.method=='GET':
+		response=HttpResponse(checkSignature(request))
+		return response
+	else:
+		return HttpResponse('Hello World')
 
 def checkSignature(request):
-    signature=request.GET.get('signature',None)
-    timestamp=request.GET.get('timestamp',None)
-    nonce=request.GET.get('nonce',None)
-    echostr=request.GET.get('echostr',None)
+	signature=request.GET.get('signature',None)
+	timestamp=request.GET.get('timestamp',None)
+	nonce=request.GET.get('nonce',None)
+	echostr=request.GET.get('echostr',None)
 
-    wechat_objs = WeChatBase.objects.all()
-    if wechat_objs:
-    	token=wechat_objs[0].wc_token
-
-	    tmplist=[token,timestamp,nonce]
-	    tmplist.sort()
-	    tmpstr="%s%s%s"%tuple(tmplist)
-	    tmpstr=hashlib.sha1(tmpstr).hexdigest()
-	    if tmpstr==signature:
-	        return echostr
-	    else:
-	        return None
+	wechat_objs = WeChatBase.objects.all()
+	if wechat_objs:
+		token = wechat_objs[0].wc_token
+		print token
+		tmplist = [token,timestamp,nonce]
+		tmplist.sort()
+		tmpstr="%s%s%s"%tuple(tmplist)
+		tmpstr=hashlib.sha1(tmpstr).hexdigest()
+		if tmpstr==signature:
+			return echostr
+		else:
+			return None
 	return None
